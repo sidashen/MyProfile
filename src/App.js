1fda5012e11d7113eb7d243672bdfae316ca240c
sidashen <shashasi8023@163.com>
2020-03-16 16:35:11 +0800

    [shasha] feat: check and submit form

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ class App extends Component {
     this.state = {
       nameValue: 'Your name',
       genderValue: 'Male',
-      descriptionValue: 'Description about yourself'
+      descriptionValue: 'Description about yourself',
+      isAgree: true
     };
   }
 
   handleChange = (event) => {
     const target = event.target;
     const name = target.name;
-    this.setState({ [name]: target.value })
+    const value = target.name === 'isAgree' ? target.checked : target.value;
+    this.setState({ [name]: value })
   }
 
   handleFocus = (event) => {
@@ -24,27 +26,53 @@ class App extends Component {
     this.setState({ [name]: '' });
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { nameValue, genderValue, descriptionValue } = this.state;
+    console.log('name: ' + nameValue
+      + ' gender: ' + genderValue
+      + ' description: ' + descriptionValue);
+  }
+
   render() {
     return (
-      <div>
+      <form onSubmit={(event) => this.handleSubmit(event)}>
         <label>
           Name
-          <input type="text" value={this.state.nameValue} onChange={(event) => this.handleChange(event)}
-            onFocus={(event) => this.handleFocus(event)} name='nameValue' />
+          <input
+            type="text"
+            value={this.state.nameValue}
+            onChange={(event) => this.handleChange(event)}
+            onFocus={(event) => this.handleFocus(event)}
+            name="nameValue" />
         </label>
         <label>
           Gender
-          <select value={this.state.genderValue} onChange={(event) => this.handleChange(event)} name='genderValue'>
-            <option value='Male'>Male</option>
-            <option value='Female'>Female</option>
+          <select
+            value={this.state.genderValue}
+            onChange={(event) => this.handleChange(event)}
+            name="genderValue">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
           </select>
         </label>
         <label>
           Description
-          <textarea value={this.state.descriptionValue} onChange={this.handleChange}
-            onFocus={(event) => this.handleFocus(event)} name='descriptionValue' />
+          <textarea
+            value={this.state.descriptionValue}
+            onChange={this.handleChange}
+            onFocus={(event) => this.handleFocus(event)} name="descriptionValue" />
         </label>
-      </div>
+        <label>
+          <input
+            name="isAgree"
+            type="checkbox"
+            checked={this.state.isAgree}
+            onChange={(event) => this.handleChange(event)} />
+          I have read the terms of conduct
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
