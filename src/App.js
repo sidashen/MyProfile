4634df14e1c368b5d98eb224c3564655779c24cb
sidashen <shashasi8023@163.com>
2020-03-16 14:05:07 +0800

    [shasha] feat: name form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 'Your name' };
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  handleFocus = () => {
+    this.setState({ value: '' });
+  }
+
+  render() {
+    return (
+      <label>
+        Name
+        <input type="text" value={this.state.value} onChange={() => this.handleChange()} onFocus={this.handleFocus} />
+      </label>
+    );
+  }
 }
 
 export default App;
